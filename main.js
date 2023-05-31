35ad2835e1399a26db2995958584db3af8870321
sergio-usma <sergioandresusma@hotmail.com>
2023-05-31 17:53:05 -0500

    Add single page app layout

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
 import BookList from './modules/bookList.js';
+import showTime from './modules/showDate.js';
+
+// Get the navigation links and sections
+const listLink = document.querySelector('.list');
+const addLink = document.querySelector('.add');
+const contactLink = document.querySelector('.contactLink');
+const booksListSection = document.querySelector('.booksList');
+const addBookSection = document.querySelector('.addBook');
+const contactSection = document.querySelector('.contact');
+
+function showBooksList(event) {
+  event.preventDefault();
+  booksListSection.style.display = 'block';
+  addBookSection.style.display = 'none';
+  contactSection.style.display = 'none';
+}
+
+function showAddBook(event) {
+  event.preventDefault();
+  booksListSection.style.display = 'none';
+  addBookSection.style.display = 'block';
+  contactSection.style.display = 'none';
+}
+
+function showContact(event) {
+  event.preventDefault();
+  booksListSection.style.display = 'none';
+  addBookSection.style.display = 'none';
+  contactSection.style.display = 'block';
+}
+
+listLink.addEventListener('click', showBooksList);
+addLink.addEventListener('click', showAddBook);
+contactLink.addEventListener('click', showContact);
 
 const bookList = new BookList();
 bookList.renderBookList();
@@ -18,3 +52,10 @@ form.addEventListener('submit', (event) => {
     bookList.renderBookList();
   }
 });
+
+const showDate = document.getElementById('showDate');
+const updateDate = showTime();
+
+setInterval(() => {
+  showDate.innerHTML = updateDate();
+}, 1000);
